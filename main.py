c044ebefe40dcb5c73a1fda6f38e13052ded6fa3
Animewala <102191216+Animewala@users.noreply.github.com>
2022-03-24 16:25:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import telegram.ext
+import os
+
+with open('token.txt', 'r') as f:
+ TOKEN = f.read()
+
+def start(update, context):
+    update.message.reply_text("hello! swagat hai")
+    
+def help(update, context):
+   update.message.reply_text("""
+   the following commands are available
+   /start -> Welcome Message
+   /help -> all helpful command list
+   /adf -> bypass adf.ly link
+   /droplink -> bypass droplink.co link
+   /sd -> AppDrive or DriveApp links (login required wont work for now)
+   /lv -> linkvertise link bypass
+
+   usage - commands link (example -->/adf https://adf.ly/xyz)
+   
+   
+   
+   
+   """)
+
+def adf(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"processing")
+        os.system('python3 bypas.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+def droplink(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"processing")
+        os.system('python3 droplink.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+def sd(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"processing")
+        os.system('python3 sd.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+def lv(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"processing")
+        os.system('python3 lv.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+updater = telegram.ext.Updater(TOKEN, use_context=True)
+disp = updater.dispatcher
+disp.add_handler(telegram.ext.CommandHandler("help", help))
+disp.add_handler(telegram.ext.CommandHandler("adf", adf))
+disp.add_handler(telegram.ext.CommandHandler("droplink", droplink))
+disp.add_handler(telegram.ext.CommandHandler("sd", sd))
+disp.add_handler(telegram.ext.CommandHandler("lv", lv))
+
+updater.start_polling()
+updater.idle()
